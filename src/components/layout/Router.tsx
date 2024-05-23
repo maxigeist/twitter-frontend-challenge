8db856424747ca2227e19149860c080f8f7c5fee
maxigeist <geist.maximo@gmail.com>
2024-05-23 11:41:03 -0300

    feat: add a component to manage private and public routes separetely. It returns you to the sign-page and if you have a token it takes you directly to the home page

--- src/components/layout/Router.tsx
+++ src/components/layout/Router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import { StyledSideBarPageWrapper } from "../../pages/side-bar-page/SideBarPageWrapper";
+import {createBrowserRouter, Outlet} from "react-router-dom";
+import {StyledSideBarPageWrapper} from "../../pages/side-bar-page/SideBarPageWrapper";
 import NavBar from "../navbar/NavBar";
 import SignUpPage from "../../pages/auth/sign-up/SignUpPage";
 import SignInPage from "../../pages/auth/sign-in/SignInPage";
@@ -10,52 +10,65 @@ import ProfilePage from "../../pages/profile/ProfilePage";
 import TweetPage from "../../pages/create-tweet-page/TweetPage";
 import CommentPage from "../../pages/create-comment-page/CommentPage";
 import PostPage from "../../pages/post-page/PostPage";
+import {PrivateRoute} from "../private-route/PrivateRoute";
+import {PublicRoute} from "../public-route/PublicRoute";
 
 const WithNav = () => {
-  return (
-    <StyledSideBarPageWrapper>
-      <NavBar />
-      <Outlet />
-    </StyledSideBarPageWrapper>
-  );
+    return (
+        <StyledSideBarPageWrapper>
+            <NavBar/>
+            <Outlet/>
+        </StyledSideBarPageWrapper>
+    );
 };
 
 export const ROUTER = createBrowserRouter([
-  {
-    path: "/sign-up",
-    element: <SignUpPage />,
-  },
-  {
-    path: "/sign-in",
-    element: <SignInPage />,
-  },
-  {
-    element: <WithNav />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/recommendations",
-        element: <RecommendationPage />,
-      },
-      {
-        path: "/profile/:id",
-        element: <ProfilePage />,
-      },
-      {
-        path: "/post/:id",
-        element: <PostPage />,
-      },
-      {
-        path: "/compose/tweet",
-        element: <TweetPage />,
-      },
-      {
-        path: "/post/:id",
-        element: <CommentPage />,
-      },
-    ],
-  },
-]);
+        {
+            element: <PublicRoute/>,
+            children: [
+                {
+                    path: "/sign-up",
+                    element: <SignUpPage/>,
+                },
+                {
+                    path: "/sign-in",
+                    element: <SignInPage/>,
+                },
+            ]
+        },
+        {
+            element: <PrivateRoute/>,
+            children: [
+                {
+                    element: <WithNav/>,
+                    children: [
+                        {
+                            path: "/",
+                            element: <HomePage/>,
+                        },
+                        {
+                            path: "/recommendations",
+                            element: <RecommendationPage/>,
+                        },
+                        {
+                            path: "/profile/:id",
+                            element: <ProfilePage/>,
+                        },
+                        {
+                            path: "/post/:id",
+                            element: <PostPage/>,
+                        },
+                        {
+                            path: "/compose/tweet",
+                            element: <TweetPage/>,
+                        },
+                        {
+                            path: "/post/:id",
+                            element: <CommentPage/>,
+                        },
+                    ],
+
+                }]
+        }
+    ])
+;
---
+++ src/components/public-route/PublicRoute.tsx
@@ -0,0 +1,12 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+
+export const PublicRoute = () => {
+    const token = localStorage.getItem("token");
+    return token ? (
+            <Navigate to="/"/>
+        ) :
+        (
+            <Outlet/>
+        )
+}
